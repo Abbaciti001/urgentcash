d5f14e7b2d82933f1ed23e846038a2405e1edf8a
Abbaciti001 <ashirupassas2@gmail.com>
2025-04-04 10:25:27 +0100

    signin page/profile page

---
+++ components/Footer.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoTiktok } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
+
+export default function Footer () {
+ const year = new Date ().getFullYear()
+    return (
+        <footer className="grid grid-cols-1 gap-4 md:px-8 md:grid-cols-2 lg:grid-cols-3 py-4 px-2 lg:gap-0">
+            <div>
+                <p className="text-2xl text-gray-800 font-bold">urgentCa$h</p>
+                <p className="text-xs text-gray-600"> &copy; {year} UrgentCa$h Limited</p>
+            </div>
+            <div>
+                <p className="text-md text-gray-700">Head office</p>
+                <p className="text-md text-gray-700">Ademola Adetokunbo crescent, Wuse 2, Abuja, Nigeria</p>
+            </div>
+            <div>
+                <ul className="flex lg:justify-end items-center gap-4">
+                   <li><Link href=""><FaFacebook className="text-lg"/></Link></li>
+                   <li><Link href=""><IoLogoTiktok  className="text-lg"/></Link></li>
+                   <li><Link href=""><FaInstagram  className="text-md"/></Link></li>
+                </ul>
+                <ul className="flex lg:justify-end items-center gap-4">
+                    <li><Link href="#" className="text-sm text-gray-600">Terms of Use</Link></li>
+                    <li><Link href="#" className="text-sm text-gray-600">Privacy Policy</Link></li>
+                    <li><Link href="#" className="text-sm text-gray-600">Bug Bounty</Link></li>
+                </ul>
+            </div>
+           
+
+        </footer>
+    )
+}
